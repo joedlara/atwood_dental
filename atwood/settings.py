25d6778b17942554f07b26617a41834b01913853
Joed Lara <joedlara18@gmail.com>
2017-09-13 17:42:56 -0600

    initial commit

--- atwood/settings.py
+++ atwood/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = ')v!xdeclx3b^n$#d1u)rx^&_+)ll5d&kq&*(3aap0)o7*ct!xa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['atwoodfamilydentist.com']
-ALLOWED_HOSTS += ip_addresses()
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
